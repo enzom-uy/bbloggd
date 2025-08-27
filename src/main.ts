bf6f940423cf4c05b2220b6918ac954fe1726307
enzom-uy <enzom.dev@gmail.com>
2025-08-27 16:18:47 -0300

    initial files

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+
+    await app.listen(process.env.PORT ?? 3000);
+
+    app.enableCors({
+        credentials: true,
+    });
+}
+bootstrap();
