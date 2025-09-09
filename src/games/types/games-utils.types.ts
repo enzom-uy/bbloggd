c4684372853b90bc85c6ed376a193d7cff5e3d11
enzom-uy <enzom.dev@gmail.com>
2025-09-09 16:38:36 -0300

    igdb types: remove useless stuff

--- src/games/types/games-utils.types.ts
+++ src/games/types/games-utils.types.ts
@@ -12,26 +12,18 @@ export interface IGDBInvolvedCompany {
     company: number;
 }
 
-export type GetInvolvedCompaniesResponse = IGDBInvolvedCompany[] | [];
-
 export interface IGDBCompany {
     id: number;
     name: string;
 }
-
-export type GetCompanyResponse = IGDBCompany[] | [];
 
 export interface IGDBReleaseDate {
     id: number;
     date: number;
 }
 
-export type GetReleaseDateResponse = IGDBReleaseDate[] | [];
-
 export interface IGDBGenre {
     id: number;
     name: string;
     slug: string;
 }
-
-export type GetGenresResponse = IGDBGenre[] | [];
